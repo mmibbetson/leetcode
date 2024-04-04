ba874f0eaffd4dad61de140ef319777d2b0ef402
Matthew Ibbetson <mmibbetson@gmail.com>
2024-04-04 22:42:02 +0200

    List -> Seq

--- fsharp/1-two-sum.fsx
+++ fsharp/1-two-sum.fsx
@@ -1,5 +1,5 @@
 let twoSum nums target =
     nums
-    |> List.indexed
-    |> List.filter (fun (idx, num) -> List.contains <| target - num <| List.removeAt idx nums)
-    |> List.map fst
+    |> Seq.indexed
+    |> Seq.filter (fun (idx, num) -> Seq.contains <| target - num <| Seq.removeAt idx nums)
+    |> Seq.map fst
---
+++ fsharp/1672-richest-customer-wealth.fsx
@@ -0,0 +1,8 @@
+module RichestCustomerWealth
+
+// My original solution, not point-free. Still concise but less descriptive
+// let maximumWealth =
+//     List.fold (fun acc cur -> let w = List.sum cur in if w > acc then w else acc) 0
+
+// Taken from Connor Hoekstra's (code_report) haskell solution
+let maximumWealth = Seq.max << Seq.map Seq.sum
