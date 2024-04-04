fe8b28184c318601a14a497a4d4b9dd76cca6303
Matthew Ibbetson <mmibbetson@gmail.com>
2024-04-04 15:49:23 +0200

    minor edit ro remove parens

--- fsharp/1-two-sum.fsx
+++ fsharp/1-two-sum.fsx
@@ -1,5 +1,5 @@
 let twoSum nums target =
     nums
     |> List.indexed
-    |> List.filter (fun (idx, number) -> List.contains (target - number) (List.removeAt idx nums))
+    |> List.filter (fun (idx, num) -> List.contains <| target - num <| List.removeAt idx nums)
     |> List.map fst
