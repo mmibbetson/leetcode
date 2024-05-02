b982fb2c5f02a45c78d99f1ad27ca1612324c500
Matthew Ibbetson <mmibbetson@gmail.com>
2024-05-02 14:25:49 +0200

    new algo

--- haskell/Easy/2529-maximum-count-of-positive-integer-and-negative-integer.hs
+++ haskell/Easy/2529-maximum-count-of-positive-integer-and-negative-integer.hs
@@ -1,6 +1,7 @@
-import Control.Monad (liftM2)
 import Control.Applicative (liftA2)
+import Control.Monad (liftM2)
 import Data.Function (on)
+import GHC.Arr (negRange)
 
 -- First solution
 maximumCount :: [Int] -> Int
@@ -26,3 +27,11 @@ maximumCount' = myCombinator max (length .: filter) (> 0) (< 0)
 -- Adapted from code_report video comment
 maximumCount'' :: [Int] -> Int
 maximumCount'' xs = (max `on` length . flip filter xs) (> 0) (< 0)
+
+-- Evan's algorithm (Still need to trim down the case for ignoring zeroes... maybe just do recursive def?)
+maxCount :: [Int] -> Int
+maxCount xs = max pos neg
+  where
+    noZeroes = filter (/= 0) xs
+    neg = length $ takeWhile (< 0) noZeroes
+    pos = length noZeroes - neg
